f7e26d7a3f7e729d61d91ce259fb52f985256923
Markus Ineichen <kusi.ineichen@gmail.com>
2025-01-25 02:22:54 +0100

    Split intrinsic into submodule

---
+++ src/calibration/intrinsic.rs
@@ -0,0 +1,202 @@
+use std::{path::Path, sync::Arc};
+
+use opencv::{
+    calib3d::{self},
+    core::{self, Mat, Vector},
+    imgproc,
+    objdetect::{
+        get_predefined_dictionary, CharucoBoard, CharucoDetector, PredefinedDictionaryType,
+    },
+    prelude::{CharucoDetectorTraitConst, *},
+};
+
+use super::{ExtrinsicCalibration, Undistorter};
+
+type VectorOfMat = Vector<Mat>;
+
+#[derive(Clone)]
+pub struct IntrinsicCalibration {
+    detector: Arc<CharucoDetector>,
+    camera_matrix: Mat,
+    dist_coeffs: Mat,
+}
+
+impl IntrinsicCalibration {
+    pub fn create_undistorter(&self, image_size: core::Size_<i32>) -> opencv::Result<Undistorter> {
+        let mut map1 = Mat::default();
+        let mut map2 = Mat::default();
+
+        // Initialize undistortion maps
+        calib3d::init_undistort_rectify_map(
+            &self.camera_matrix,
+            &self.dist_coeffs,
+            &Mat::default(),
+            &self.camera_matrix, // Use same camera matrix for simplicity
+            image_size,
+            core::CV_32FC1,
+            &mut map1,
+            &mut map2,
+        )?;
+        Ok(Undistorter { map1, map2 })
+    }
+    pub fn camera_matrix(&self) -> &Mat {
+        &self.camera_matrix
+    }
+
+    pub fn dist_coeffs(&self) -> &Mat {
+        &self.dist_coeffs
+    }
+
+    pub fn create(
+        images: impl IntoIterator<Item = opencv::Result<Mat>>,
+    ) -> Result<IntrinsicCalibration, opencv::Error> {
+        // Directory containing calibration images
+
+        let square_size = 29.0; // mm
+        let marker_size = 14.0; // mm
+
+        // Create ChArUco board
+
+        let dictionary = get_predefined_dictionary(PredefinedDictionaryType::DICT_5X5_1000)?;
+        let board = CharucoBoard::new_def(
+            opencv::core::Size::new(5, 7), // board dimensions
+            square_size,
+            marker_size, // marker size
+            &dictionary,
+        )?;
+        let detector = CharucoDetector::new_def(&board)?;
+        // Collect all image paths
+
+        let mut all_object_points = VectorOfMat::new();
+        let mut all_image_points = VectorOfMat::new();
+
+        // Process each image
+        let images = images
+            .into_iter()
+            .map(|img| {
+                let img = img?;
+
+                let mut gray = Mat::default();
+                opencv::imgproc::cvt_color(
+                    &img,
+                    &mut gray,
+                    imgproc::COLOR_BGR2GRAY,
+                    0,
+                    core::AlgorithmHint::ALGO_HINT_DEFAULT,
+                )?;
+
+                let mut corners = Mat::default();
+                let mut ids = Mat::default();
+
+                // Detect markers using ArucoDetector
+                detector.detect_board_def(&gray, &mut corners, &mut ids)?;
+                if !ids.empty() {
+                    let mut object_points = Mat::default();
+                    let mut image_points = Mat::default();
+                    board.match_image_points(
+                        &corners,
+                        &ids,
+                        &mut object_points,
+                        &mut image_points,
+                    )?;
+
+                    all_object_points.push(object_points);
+                    all_image_points.push(image_points);
+                }
+
+                opencv::Result::Ok(img)
+            })
+            .collect::<opencv::Result<Vec<_>>>()?;
+
+        let image_size = images[0].size()?;
+        let mut camera_matrix = Mat::default();
+        let mut dist_coeffs = Mat::default();
+        let mut rvecs = VectorOfMat::new();
+        let mut tvecs = VectorOfMat::new();
+
+        let flags = calib3d::CALIB_FIX_K2
+            | calib3d::CALIB_FIX_K3
+            | calib3d::CALIB_FIX_K4
+            | calib3d::CALIB_FIX_K5
+            | calib3d::CALIB_FIX_K6
+            | calib3d::CALIB_ZERO_TANGENT_DIST
+            | calib3d::CALIB_USE_LU;
+
+        let error = calib3d::calibrate_camera(
+            &all_object_points,
+            &all_image_points,
+            image_size,
+            &mut camera_matrix,
+            &mut dist_coeffs,
+            &mut rvecs,
+            &mut tvecs,
+            flags,
+            core::TermCriteria::default()?,
+        )?;
+        println!("Calibration error: {}", error);
+        println!("Camera matrix:\n{:?}", camera_matrix);
+        println!("Distortion coefficients:\n{:?}", dist_coeffs);
+
+        // Tested 22.01. Improved quality
+        let camera_matrix = calib3d::get_optimal_new_camera_matrix_def(
+            &camera_matrix,
+            &dist_coeffs,
+            image_size,
+            0.0, // alpha=0 to crop all black pixels
+        )?;
+
+        Ok(Self {
+            detector: Arc::new(detector),
+            camera_matrix,
+            dist_coeffs,
+        })
+    }
+
+    pub fn calibrate_extrinsic(
+        self,
+        distorted: &Mat,
+    ) -> Result<ExtrinsicCalibration, opencv::Error> {
+        let image_size = distorted.size()?;
+        let mut corners = Mat::default();
+        let mut ids = Mat::default();
+        // Detect markers using ArucoDetector
+        self.detector
+            .detect_board_def(distorted, &mut corners, &mut ids)?;
+        if ids.empty() {
+            return Err(opencv::Error::new(
+                core::StsObjectNotFound,
+                "Nothing detected",
+            ));
+        }
+        let mut object_points = Mat::default();
+        let mut image_points = Mat::default();
+        self.detector.get_board()?.match_image_points(
+            &corners,
+            &ids,
+            &mut object_points,
+            &mut image_points,
+        )?;
+
+        // Estimate pose using solvePnP
+        let mut rvec = Mat::default();
+        let mut tvec = Mat::default();
+
+        calib3d::solve_pnp(
+            &object_points,
+            &image_points,
+            &self.camera_matrix,
+            &self.dist_coeffs,
+            &mut rvec,
+            &mut tvec,
+            false,
+            calib3d::SOLVEPNP_ITERATIVE,
+        )?;
+
+        Ok(ExtrinsicCalibration {
+            intrinsic: self,
+            rvec,
+            tvec,
+            image_size,
+        })
+    }
+}
--- src/calibration/mod.rs
+++ src/calibration/mod.rs
@@ -1,29 +1,18 @@
-use std::{path::Path, sync::Arc};
+use std::path::Path;
 
 use opencv::{
     calib3d::{self},
-    core::{self, Mat, Point2f, Point3f, Size_, Vector},
+    core::{self, Mat, MatTraitConst, Point2f, Point3f, Size_, Vector},
     imgproc,
-    objdetect::{
-        get_predefined_dictionary, CharucoBoard, CharucoDetector, PredefinedDictionaryType,
-    },
-    prelude::{CharucoDetectorTraitConst, *},
 };
 
+mod intrinsic;
 mod pixel_to_world;
 
+pub use intrinsic::*;
 pub use pixel_to_world::*;
 
-type VectorOfMat = Vector<Mat>;
-
 pub enum CalibrationError {}
-
-#[derive(Clone)]
-pub struct IntrinsicCalibration {
-    detector: Arc<CharucoDetector>,
-    camera_matrix: Mat,
-    dist_coeffs: Mat,
-}
 
 pub struct ExtrinsicCalibration {
     intrinsic: IntrinsicCalibration,
@@ -88,186 +77,13 @@ impl Undistorter {
     }
 }
 
-impl IntrinsicCalibration {
-    pub fn create_undistorter(&self, image_size: core::Size_<i32>) -> opencv::Result<Undistorter> {
-        let mut map1 = Mat::default();
-        let mut map2 = Mat::default();
-
-        // Initialize undistortion maps
-        calib3d::init_undistort_rectify_map(
-            &self.camera_matrix,
-            &self.dist_coeffs,
-            &Mat::default(),
-            &self.camera_matrix, // Use same camera matrix for simplicity
-            image_size,
-            core::CV_32FC1,
-            &mut map1,
-            &mut map2,
-        )?;
-        Ok(Undistorter { map1, map2 })
-    }
-
-    pub fn create(
-        images: impl IntoIterator<Item = opencv::Result<Mat>>,
-    ) -> Result<IntrinsicCalibration, opencv::Error> {
-        // Directory containing calibration images
-
-        let square_size = 29.0; // mm
-        let marker_size = 14.0; // mm
-
-        // Create ChArUco board
-
-        let dictionary = get_predefined_dictionary(PredefinedDictionaryType::DICT_5X5_1000)?;
-        let board = CharucoBoard::new_def(
-            opencv::core::Size::new(5, 7), // board dimensions
-            square_size,
-            marker_size, // marker size
-            &dictionary,
-        )?;
-        let detector = CharucoDetector::new_def(&board)?;
-        // Collect all image paths
-
-        let mut all_object_points = VectorOfMat::new();
-        let mut all_image_points = VectorOfMat::new();
-
-        // Process each image
-        let images = images
-            .into_iter()
-            .map(|img| {
-                let img = img?;
-
-                let mut gray = Mat::default();
-                opencv::imgproc::cvt_color(
-                    &img,
-                    &mut gray,
-                    imgproc::COLOR_BGR2GRAY,
-                    0,
-                    core::AlgorithmHint::ALGO_HINT_DEFAULT,
-                )?;
-
-                let mut corners = Mat::default();
-                let mut ids = Mat::default();
-
-                // Detect markers using ArucoDetector
-                detector.detect_board_def(&gray, &mut corners, &mut ids)?;
-                if !ids.empty() {
-                    let mut object_points = Mat::default();
-                    let mut image_points = Mat::default();
-                    board.match_image_points(
-                        &corners,
-                        &ids,
-                        &mut object_points,
-                        &mut image_points,
-                    )?;
-
-                    all_object_points.push(object_points);
-                    all_image_points.push(image_points);
-                }
-
-                opencv::Result::Ok(img)
-            })
-            .collect::<opencv::Result<Vec<_>>>()?;
-
-        let image_size = images[0].size()?;
-        let mut camera_matrix = Mat::default();
-        let mut dist_coeffs = Mat::default();
-        let mut rvecs = VectorOfMat::new();
-        let mut tvecs = VectorOfMat::new();
-
-        let flags = calib3d::CALIB_FIX_K2
-            | calib3d::CALIB_FIX_K3
-            | calib3d::CALIB_FIX_K4
-            | calib3d::CALIB_FIX_K5
-            | calib3d::CALIB_FIX_K6
-            | calib3d::CALIB_ZERO_TANGENT_DIST
-            | calib3d::CALIB_USE_LU;
-
-        let error = calib3d::calibrate_camera(
-            &all_object_points,
-            &all_image_points,
-            image_size,
-            &mut camera_matrix,
-            &mut dist_coeffs,
-            &mut rvecs,
-            &mut tvecs,
-            flags,
-            core::TermCriteria::default()?,
-        )?;
-        println!("Calibration error: {}", error);
-        println!("Camera matrix:\n{:?}", camera_matrix);
-        println!("Distortion coefficients:\n{:?}", dist_coeffs);
-
-        // Tested 22.01. Improved quality
-        let camera_matrix = calib3d::get_optimal_new_camera_matrix_def(
-            &camera_matrix,
-            &dist_coeffs,
-            image_size,
-            0.0, // alpha=0 to crop all black pixels
-        )?;
-
-        Ok(Self {
-            detector: Arc::new(detector),
-            camera_matrix,
-            dist_coeffs,
-        })
-    }
-
-    pub fn calibrate_extrinsic(
-        self,
-        distorted: &Mat,
-    ) -> Result<ExtrinsicCalibration, opencv::Error> {
-        let image_size = distorted.size()?;
-        let mut corners = Mat::default();
-        let mut ids = Mat::default();
-        // Detect markers using ArucoDetector
-        self.detector
-            .detect_board_def(distorted, &mut corners, &mut ids)?;
-        if ids.empty() {
-            return Err(opencv::Error::new(
-                core::StsObjectNotFound,
-                "Nothing detected",
-            ));
-        }
-        let mut object_points = Mat::default();
-        let mut image_points = Mat::default();
-        self.detector.get_board()?.match_image_points(
-            &corners,
-            &ids,
-            &mut object_points,
-            &mut image_points,
-        )?;
-
-        // Estimate pose using solvePnP
-        let mut rvec = Mat::default();
-        let mut tvec = Mat::default();
-
-        calib3d::solve_pnp(
-            &object_points,
-            &image_points,
-            &self.camera_matrix,
-            &self.dist_coeffs,
-            &mut rvec,
-            &mut tvec,
-            false,
-            calib3d::SOLVEPNP_ITERATIVE,
-        )?;
-
-        Ok(ExtrinsicCalibration {
-            intrinsic: self,
-            rvec,
-            tvec,
-            image_size,
-        })
-    }
-}
-
 impl ExtrinsicCalibration {
     fn build_world_to_pixel(&self) -> opencv::Result<PixelToWorldLut> {
         let transformer = PixelToWorldTransformer::new(
-            &self.intrinsic.camera_matrix,
+            self.camera_matrix(),
             &self.rvec,
             &self.tvec,
-            &self.intrinsic.dist_coeffs,
+            &self.dist_coeffs(),
         )?;
         let time = std::time::Instant::now();
         let lut = PixelToWorldLut::new(
@@ -317,12 +133,19 @@ impl ExtrinsicCalibration {
             &points_world,
             &self.rvec,
             &self.tvec,
-            &self.intrinsic.camera_matrix,
-            &self.intrinsic.dist_coeffs,
+            &self.camera_matrix(),
+            &self.dist_coeffs(),
             &mut projected_points,
         )?;
 
         Ok(projected_points)
+    }
+
+    pub fn camera_matrix(&self) -> &Mat {
+        self.intrinsic.camera_matrix()
+    }
+    pub fn dist_coeffs(&self) -> &Mat {
+        self.intrinsic.dist_coeffs()
     }
 }
 
