1a612d5b6fdbe6b248605cee48ac106b67add35e
Markus Ineichen <kusi.ineichen@gmail.com>
2026-01-08 11:50:52 +0100

    Update to latest

--- pilatus-opencv/src/image.rs
+++ pilatus-opencv/src/image.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 
-use imbuf::{DynamicImageChannel, ImageChannel, LumaImage, PixelType};
+use imbuf::{ImageChannel, PixelType};
 use opencv::{boxed_ref::BoxedRef, core::Mat};
 use pilatus_engineering::image::{DynamicImage, Image};
 
@@ -45,8 +45,8 @@ impl<'mat, 'img: 'mat, T: PixelType + 'static + opencv::core::DataType>
     fn try_from(img: &'img ImageChannel<T>) -> Result<Self, Self::Error> {
         let (width, height) = img.dimensions();
         Ok(BorrowImage(Mat::new_rows_cols_with_data(
-            width.get() as _,
             height.get() as _,
+            width.get() as _,
             img.buffer() as _,
         )?))
     }
@@ -59,9 +59,50 @@ impl<'mat, 'img: 'mat, T: PixelType + 'static + opencv::core::DataType> TryFrom<
     fn try_from(img: &'img Image<T, 1>) -> Result<Self, Self::Error> {
         let (width, height) = img.dimensions();
         Ok(BorrowImage(Mat::new_rows_cols_with_data(
-            width.get() as _,
             height.get() as _,
+            width.get() as _,
             img.buffer() as _,
         )?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroU32;
+
+    use opencv::prelude::MatTraitConst;
+    use opencv::prelude::MatTraitConstManual;
+
+    use super::*;
+
+    fn imbuf_image_to_borrowed_mat_preserves_dims_and_content<T>(pixels: Vec<T>)
+    where
+        T: PixelType + opencv::core::DataType + std::fmt::Debug + PartialEq + 'static,
+    {
+        let width = NonZeroU32::new(3).unwrap();
+        let height = NonZeroU32::new(2).unwrap();
+        let img: Image<T, 1> = Image::new_vec(pixels, width, height);
+
+        let borrowed: BorrowImage<'_> = (&img).try_into().unwrap();
+        let mat = &*borrowed;
+
+        assert_eq!(img.width(), width);
+        assert_eq!(img.height(), height);
+        assert_eq!(mat.rows(), height.get() as i32);
+        assert_eq!(mat.cols(), width.get() as i32);
+
+        let mat_data = mat.data_typed::<T>().unwrap();
+        assert_eq!(mat_data, img.buffer());
+        assert_eq!(mat_data.as_ptr() as usize, img.buffer().as_ptr() as usize);
+    }
+
+    #[test]
+    fn imbuf_image_u8_to_borrowed_mat_preserves_dims_and_content() {
+        imbuf_image_to_borrowed_mat_preserves_dims_and_content(vec![0u8, 1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn imbuf_image_u16_to_borrowed_mat_preserves_dims_and_content() {
+        imbuf_image_to_borrowed_mat_preserves_dims_and_content(vec![10u16, 20, 30, 40, 50, 60]);
     }
 }
